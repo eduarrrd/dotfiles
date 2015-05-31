dca535f498a3a30ad1247893b01391b133cde519
Eduard Bachmakov <e.bachmakov@gmail.com>
2015-05-31 14:51:42 -0400

    Add/update nix environments

--- nix/.nixpkgs/config.nix
+++ nix/.nixpkgs/config.nix
@@ -17,11 +17,23 @@
       };
       scienceEnv = pkgs.myEnvFun {
           name = "sci";
-          buildInputs = with python27Packages;[ ipython pandas sympy scipy ];
+          buildInputs = with python27Packages; [ ipython pandas sympy scipy ];
       };
       haskellEnv = pkgs.myEnvFun {
           name = "haskell";
+          buildInputs = with haskellPackages; [ ghc ];
+      };
+      hakyllEnv = pkgs.myEnvFun {
+          name = "hakyll";
+          buildInputs = with haskellPackages; [ ghc hakyll ];
+      };
+      yesodEnv = pkgs.myEnvFun {
+          name = "yesod";
           buildInputs = with haskellPackages; [ ghc gtk2hsBuildtools openssl yesod cabalInstall ];
+      };
+      osEnv = pkgs.myEnvFun {
+          name = "os";
+          buildInputs = with pkgs; [ gcc nasm qemu bochs ];
       };
       xmonadEnv = pkgs.myEnvFun {
           name = "xmonad";
